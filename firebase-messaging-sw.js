76babc0f14fcb2b479a05cf77e0b2a75ab95ec82
Mucopp <m.z.f.r.g.u.r+is@gmail.com>
2026-05-23 17:00:07 +0300

    firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -41,7 +41,7 @@ self.addEventListener('notificationclick', event => {
 });
 
 // Statik cache
-const CACHE_NAME = 'cyclemaster-v16';
+const CACHE_NAME = 'cyclemaster-v22';
 const STATIC_ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', event => {
