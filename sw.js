a0268333745872bf2b8a380313a9bcec1dc4a971
Mucopp <m.z.f.r.g.u.r+is@gmail.com>
2026-05-25 05:21:49 +0300

    sw.js

--- sw.js
+++ sw.js
@@ -41,7 +41,7 @@ self.addEventListener('notificationclick', event => {
 });
 
 // Statik cache
-const CACHE_NAME = 'cyclemaster-v26';
+const CACHE_NAME = 'cyclemaster-v27';
 const STATIC_ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', event => {
